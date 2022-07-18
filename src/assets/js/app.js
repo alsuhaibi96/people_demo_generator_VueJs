cec22a71d188c2bdbfca795d6edf5ec8845100a5
Alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2022-07-18 10:29:58 +0300

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,29 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            firstName: 'Abdulrahman',
+            lastName: 'Alsuhaibi',
+            email: 'alsuhaibi@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+
+        }
+
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+                // console.log(typeof(results));
+        }
+    }
+
+}).mount('#app')
